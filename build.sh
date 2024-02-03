3d7d453657151503e16f75b43a942d6178b30719
Michael Ramstein <michael@ramste.in>
2024-02-03 12:50:35 +0100

    Extracts musl from dockcross

--- build.sh
+++ build.sh
@@ -8,4 +8,10 @@ readonly OTP_VERSION="26.2.1"
 ./dockcross-linux-x64 ./build-erlang-bootstrap.sh
 ./dockcross-linux-armv7l-musl ./build-erlang.sh
 
+# TODO: Check patching of musl
+# /usr/xcc/armv7l-linux-musleabihf-cross/armv7l-linux-musleabihf/lib/ld-musl-armhf.so.1
+# TODO: Extract and ship musl?
+# ./dockcross-linux-armv7l-musl cp /usr/xcc/armv7l-linux-musleabihf-cross/armv7l-linux-musleabihf/lib/ld-musl-armhf.so.1 ld-musl-armhf.so.1
+./dockcross-linux-armv7l-musl cp /usr/xcc/armv7l-linux-musleabihf-cross/armv7l-linux-musleabihf/lib/libc.so ld-musl-armhf.so.1
+
 ./package.sh
