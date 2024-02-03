e8a9c1258e2030085cba1e5e3a0a4830ad1ca42f
Michael Ramstein <michael@ramste.in>
2024-02-03 13:04:03 +0100

    Adds github action

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-readonly OPENSSL_VERSION="3.2.1"
-readonly OTP_VERSION="26.2.1"
+OPENSSL_VERSION="${OPENSSL_VERSION:-"3.2.1"}"
+OTP_VERSION="${OTP_VERSION:-"26.2.1"}"
 
 ./dockcross-linux-armv7l-musl ./build-openssl.sh
 ./dockcross-linux-x64 ./build-erlang-bootstrap.sh
@@ -13,5 +13,3 @@ readonly OTP_VERSION="26.2.1"
 # TODO: Extract and ship musl?
 # ./dockcross-linux-armv7l-musl cp /usr/xcc/armv7l-linux-musleabihf-cross/armv7l-linux-musleabihf/lib/ld-musl-armhf.so.1 ld-musl-armhf.so.1
 ./dockcross-linux-armv7l-musl cp /usr/xcc/armv7l-linux-musleabihf-cross/armv7l-linux-musleabihf/lib/libc.so ld-musl-armhf.so.1
-
-./package.sh
